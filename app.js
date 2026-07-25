4edfc67326dd327a4cf902e5338e9ec028d5687f
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2026-07-25 19:41:21 +0700

    server setup

---
+++ app.js
@@ -0,0 +1,19 @@
+// Dependencies
+const express = require("express")
+const cors = require("cors")
+
+// Resource
+const corsOptions = require("./config/cors")
+
+// Entry
+const app = express()
+
+// Middleware
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+module.exports = app;
